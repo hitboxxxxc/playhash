b4456415dee4a30f15398e3c97570d3ee0886053
hitboxxxxc <felisqa@gmail.com>
2026-08-24 01:18:49 -0300

    fix(auth): mapeamento especifico PT-BR para erros de autenticacao

--- lib/core/services/auth_error_messages.dart
+++ lib/core/services/auth_error_messages.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/services.dart' show PlatformException;
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 /// Mensagens de erro de autenticação SEGURAS em PT-BR.
